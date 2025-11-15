f63ce6a2c4d6528d95fbcdcc9b6cd558f966be01
Rolex-444 <sivaraman.gc49@gmail.com>
2025-11-15 09:15:47 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,11 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from datetime import datetime, timedelta, timezone
 from typing import Optional, Dict
 import logging
-from config import MONGO_URI, VERIFICATION_FREE_LIMIT, VERIFICATION_PERIOD_HOURS
+from config import (
+    MONGO_URI,
+    VERIFICATION_FREE_LIMIT,
+    VERIFICATION_PERIOD_HOURS,
+)
 
 logger = logging.getLogger(__name__)
 
